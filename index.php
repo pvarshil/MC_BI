1ae171cc1a54ae0d321131cd4989298248b093eb
pvarshil <varshilcj@gmail.com>
2024-07-22 15:43:57 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-/* #########################
-* This code was developed by:
-* Audox Ingeniería SpA.
-* website: www.audox.com
-* email: info@audox.com
-######################### */
-
 include 'auth.php';
 
 function getRecords($endpoint, $params){
